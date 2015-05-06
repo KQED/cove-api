f21460f92c8b6fc7fb67b6895de3d6f5b013e90b
Kip Gebhardt <kgebhardt@kqed.org>
2015-05-05 17:46:46 -0700

    KIP-1840: Adding api_id to URL. Unit Test

--- test/mocha-tests.js
+++ test/mocha-tests.js
@@ -3,11 +3,18 @@ var demand  = require('must'),
     moment  = require('moment'),
     COVEApi = require('./../');
 
+var api_id =  process.env.COVE_API_ID || null,
+    api_secret = process.env.COVE_API_SECRET || null;
 
 var options = {
-    api_id          : process.env.COVE_API_ID || null,
-    api_secret      : process.env.COVE_API_SECRET || null,
+    api_id          : api_id,
+    api_secret      : api_secret,
     log_level       : 'debug'
+};
+
+var dummy_options = {
+    api_id          : 'TEST123',
+    api_secret      : 'TEST456'
 };
 
 if (options.api_id === null) {
@@ -30,13 +37,18 @@ describe("COVE API ", function() {
         nonce.must.have.length(32);
     });
 
+    it("Should allow api_id and api_secret to be set", function (){
+        coveAPI.setAuth(dummy_options);
+        coveAPI.api_id.must.equal(dummy_options.api_id);
+        coveAPI.api_secret.must.equal(dummy_options.api_secret);
+    });
+
     it("Should properly normalize a url", function (){
         var url = 'http://api.pbs.org/cove/v1/videos/?filter_nola_root=SOTM&filter_mediafile_set__video_encoding__mime_type=application/x-mpegURL&fields=tp_media_object_id,title,associated_images',
-            correctly_normalized = 'http://api.pbs.org/cove/v1/videos/?fields=tp_media_object_id%252Ctitle%252Cassociated_images&filter_mediafile_set__video_encoding__mime_type=application%252Fx-mpegURL&filter_nola_root=SOTM';
+            correctly_normalized = 'http://api.pbs.org/cove/v1/videos/?consumer_key=TEST123&fields=tp_media_object_id%252Ctitle%252Cassociated_images&filter_mediafile_set__video_encoding__mime_type=application%252Fx-mpegURL&filter_nola_root=SOTM&nonce=66486f690b8030fa661e315749ff5d95&timestamp=1430872773';
 
-        var normalized_url = coveAPI.normalizeUrl(url);
+        var normalized_url = coveAPI.normalizeUrl(url, '1430872773', '66486f690b8030fa661e315749ff5d95');
 
         normalized_url.must.be.equal.to(correctly_normalized);
     });
-
 });
